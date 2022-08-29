af407292c3c7cc122ad62e0e76b64f0b07d7fb00
Kim-Lauwers <lauwers.kim@gmail.com>
2022-08-29 11:27:31 +0200

    Multiply

--- projects/calculator/calculator.py
+++ projects/calculator/calculator.py
@@ -2,3 +2,7 @@ class Calculator:
     @staticmethod
     def sum(number1: float, number2: float) -> float:
         return number1 + number2
+
+    @staticmethod
+    def multiply(number1: float, number2: float) -> float:
+        return number1 * number2
--- projects/calculator/tests/calculator_tests.py
+++ projects/calculator/tests/calculator_tests.py
@@ -13,3 +13,13 @@ class CalculatorTestCase(unittest.TestCase):
         self.assertEqual(Calculator.sum(5.5, 5), 10.5)
         self.assertEqual(Calculator.sum(6, 5.5), 11.5)
         self.assertEqual(Calculator.sum(6.5, 7.5), 14)
+
+    def test_multiply_calculator_integer(self):
+        self.assertEqual(Calculator.multiply(5, 5), 25)
+        self.assertEqual(Calculator.multiply(4, 5), 20)
+        self.assertEqual(Calculator.multiply(7, 7), 49)
+
+    def test_multiply_calculator_float(self):
+        self.assertEqual(Calculator.multiply(5.5, 5), 27.5)
+        self.assertEqual(Calculator.multiply(6, 5.5), 33)
+        self.assertEqual(Calculator.multiply(6.5, 7.5), 48.75)
