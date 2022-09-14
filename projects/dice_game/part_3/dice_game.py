c45ee9ea6b7a4e1ef51cbb8162f8117c1a8bdab3
Kim-Lauwers <lauwers.kim@gmail.com>
2022-09-14 10:04:00 +0200

    Dice Game - part 3

--- projects/dice_game/part_3/dice_game.py
+++ projects/dice_game/part_3/dice_game.py
@@ -2,25 +2,70 @@ from projects.dice_game.part_3.dice import Dice
 
 
 class DiceGame:
-    def __init__(self):
-        self.__dices: list[Dice] = []
-        for i in range(5):
-            self.__dices.append(Dice())
-
     def play(self):
+        __dices: list[Dice] = []
+        for i in range(5):
+            __dices.append(Dice())
+
         print("You got:")
-        self.play_round(str(1))
-        self.play_round(str(2))
-        self.play_round(str(3))
+        __total_score = 0
+        __dices, round_score = self.play_round(str(1), __dices)
+        __total_score += round_score
 
-    def play_round(self, round_name):
+        __dices, round_score = self.play_round(str(2), __dices)
+        __total_score += round_score
+
+        __dices, round_score = self.play_final_round(str(3), __dices)
+        __total_score += round_score
+
+        print("Totale score: " + str(__total_score))
+
+    def play_round(self, round_name: str, dices: list[Dice]):
+        return self.__play_round(round_name, dices, False)
+
+    def play_final_round(self, round_name: str, dices: list[Dice]):
+        return self.__play_round(round_name, dices, True)
+
+    def __play_round(self, round_name: str, dices: list[Dice], final_round: bool):
         print("round " + round_name + ":")
-        for current_dice in range(len(self.__dices)):
-            print("Dice " + str(current_dice + 1) + ": " + str(self.__dices[current_dice].roll()))
 
-        for current_dice in range(len(self.__dices)):
-            for i in range(len(self.__dices)):
-                if i != current_dice and self.__dices[current_dice].get_value() == self.__dices[i].get_value():
-                    self.__dices[i].lock()
-                    self.__dices[current_dice].lock()
-        self.__dices[:] = filter(lambda dice: dice.can_be_rolled(), self.__dices)
+        if not dices:
+            print("No more dices left to play")
+            return []
+        else:
+            self.__roll_dices(dices)
+            self.__lock_dices(dices)
+            if final_round:
+                score = self.__count_score_eyes(dices)
+            else:
+                score = self.__count_score_double_eyes(dices)
+            return list(filter(lambda dice: dice.can_be_rolled(), dices)), score
+
+    @classmethod
+    def __count_score_eyes(cls, dices) -> int:
+        score = 0
+        for dice in dices:
+            score += dice.get_value()
+
+        return score
+
+    @staticmethod
+    def __count_score_double_eyes(dices) -> int:
+        score = 0
+        for dice in dices:
+            if not dice.can_be_rolled():
+                score += dice.get_value()
+        return score
+
+    @staticmethod
+    def __lock_dices(dices: list[Dice]):
+        for current_dice in range(len(dices)):
+            for i in range(len(dices)):
+                if i != current_dice and dices[current_dice].get_value() == dices[i].get_value():
+                    dices[i].lock()
+                    dices[current_dice].lock()
+
+    @staticmethod
+    def __roll_dices(dices: list[Dice]):
+        for current_dice in range(len(dices)):
+            print("Dice " + str(current_dice + 1) + ": " + str(dices[current_dice].roll()))
